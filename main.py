94c2f1d6db7c8a7f755f41b1b455a4acd69a9fbb
Developer-Fan <81670490+Developer-Fan@users.noreply.github.com>
2022-12-01 00:21:24 +0000

    stuff

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import random
 import math
 
-for i in range(100):
+stuff = {}
+
+for i in range(1000):
     boats = 2
     pots = 5
     money = 0
     for j in range(15):
+        num_pots = 0
+        num_boats = 0
         in_pots = random.randint(0, pots)
         out_pots = pots - in_pots
         weather = random.choice([
@@ -20,13 +24,33 @@ for i in range(100):
         num_boats = math.floor(money/100)
         boats += num_boats
         money -= num_boats*100
-        if pots*10 <= boats:
+        if pots <= boats*10:
             num_pots = math.floor(money/5)
             pots += num_pots
             money -= num_pots * 5
-        print(f"""t
-        Boats: {boats}
-        New Boats: {num_boats}
+        #print(f"""
+        #Boats: {boats}
+        #New Boats: {num_boats}
+        #Pots: {pots}
+        #New Pots: {num_pots}
+        #Money: {money}
+        #""")
+    journey = {
+        "boats": boats,
+        "pots": pots,
+        "money": money
+    }
+    stuff[str(i)] = journey
 
-        """)
+money_journey = []
+
+for i in stuff:
+    money_journey.append(
+        stuff[i]["boats"]*100+
+        stuff[i]["pots"]*5+
+        stuff[i]["money"]
+    )
+
+money_journey.sort()
+print(money_journey[999])
             
